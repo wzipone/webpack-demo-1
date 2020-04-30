3d6f201242dd79ef6c984ac5e1e1da81072354cb
wzipone <wzipone76@gmail.com>
2020-04-30 16:38:39 +0800

    使用style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = [{
     mode: 'development',
+    devtool :"inline-source-map",
+    devServer:{
+        contentBase:'./dist'
+    },
     entry: "./src", // string | object | array
     // defaults to ./src/index.js 默认入口路径 
     output: {
@@ -10,10 +15,22 @@ module.exports = [{
         // 输出路径 默认是 dist
         // the target directory for all output files
         // must be an absolute path (use the Node.js path module)
-        // filename: "index.js", // sting
+        filename: "index.[contenthash].js", 
         //输出文件名 默认是main.js
         // the filename template for entry chunks
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title:"wzp webpack demo",
+        template: 'src/assets/test.html'
+    })],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'] // 用css-loader读遇到的css文件（上面的正则匹配），然后交给style-loader处理(在html中写到style标签里)
+          }
+        ],
+      }
 },{
     mode: 'development',
     entry: "./src/index2.js", // string | object | array
